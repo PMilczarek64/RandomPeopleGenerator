b3bc98e3c1e90438ce739a15ffe9d279248d915d
Mammoth64 <mammoth1907@gmail.com>
2023-06-06 22:54:32 +0200

    Add random email and phone number generation

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const genders = ['male', 'female'];
-const maleNames = ['Jack', 'Jacob', 'Stephan'];
-const femaleNames = ['Barbara', 'Stephannie', 'June'];
-const lastNames = ['Kowalski', 'Eastwood', 'Johnson'];
+const genders = ["male", "female"];
+const maleNames = ["Jack", "Jacob", "Stephan", "John", "Robert", "William"];
+const femaleNames = ["Barbara", "Stephannie", "June", "Linda", "Mary", "Patricia"];
+const lastNames = ["Kowalski", "Eastwood", "Johnson", "Smith", "Brown", "Lewandowski"];
+const emailDomains = ["gmail.com", "yahoo.com", "outlook.com"];
 const people = [];
 
-const randChoice = (arr) => {
+const randArrChoice = (arr) => {
   const randomIndex = Math.floor(Math.random() * arr.length);
   return arr[randomIndex];
 };
@@ -14,22 +15,41 @@ const randChoice = (arr) => {
 const randomAge = (minAge, maxAge) => {
   const age = Math.floor(Math.random() * (maxAge - minAge + 1) + minAge);
   return age;
-}
+};
 
-for(let i = 0; i < 20; i++) {
-  const gender = randChoice(genders);
+const randomPhone = () => {
+  let phone = "";
+  for (let i = 0; i < 9; i++) {
+    const newCipher = Math.floor(Math.random() * 10);
+    phone += newCipher;
+
+    if ((i + 1) % 3 === 0 && i !== 8) {
+      phone += "-";
+    };
+  };
+  return phone;
+};
+
+for (let i = 0; i < 20; i++) {
+  const gender = randArrChoice(genders);
+  const firstName =
+    gender === "male" ? randArrChoice(maleNames) : randArrChoice(femaleNames);
+  const lastName = randArrChoice(lastNames);
+  const email = `${firstName}.${lastName}@${randArrChoice(emailDomains)}`;
   const newPerson = {
     gender: gender,
-    firstName: gender === 'male' ? randChoice(maleNames) : randChoice(femaleNames),
-    lastName: randChoice(lastNames),
+    firstName: firstName,
+    lastName: lastName,
     age: randomAge(18, 100),
+    email: email.toLowerCase(),
+    phoneNumber: randomPhone(),
   };
   people.push(newPerson);
-};
+}
 
 const typedArr = JSON.stringify(people);
 
-fs.writeFile('people.json', typedArr, (err) => {
+fs.writeFile("people.json", typedArr, (err) => {
   if (err) throw err;
-  console.log('The file has been saved!');
+  console.log("The file has been saved!");
 });
